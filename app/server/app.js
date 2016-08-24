80582c25f6bab09647158e194a0bb7b8afd2435c
Chris Bates-Keegan <chris@evansfinch.co.uk>
2016-08-24 17:29:04 +0100

    - ADDED: load the blocklist.

--- app/server/app.js
+++ app/server/app.js
@@ -14,14 +14,14 @@ var express = require('express');
 var methodOverride = require('method-override');
 var bodyParser = require('body-parser');
 var session = require('express-session');
-// var MongoStore = require('connect-mongo')(session);
 var LevelStore = require('level-session-store')(session);
 var morgan = require('morgan');
 var Logging = require('./logging');
 var Config = require('./config');
 var passport = require('passport');
 var auth = require('./auth');
-var tools = require('./tools');
+var twitter = require('./twitter');
+var rest = require('restler');
 
 /**
  * Express
@@ -29,23 +29,11 @@ var tools = require('./tools');
 var app = module.exports = express();
 var _env = app.get('env');
 var _map = {
-  'development': 'dev',
-  'production': 'prod',
-  'test': 'test'
+  development: 'dev',
+  production: 'prod',
+  test: 'test'
 };
 Config.env = _map[_env];
-
-
-// var mongoStoreSettings = {
-//   url: `mongodb://${Config.mongoUrl[_map[_env]]}/${Config.app.code}-sessions`,
-//   db: `${Config.app.code}-sessions`,
-//   host: Config.mongoUrl[_map[_env]],
-//   collect: 'sessions'
-// };
-
-var levelStoreSettings = {
-
-};
 
 /**
  * Configuration
@@ -74,17 +62,16 @@ var configureApp = env => {
   app.use(session({
     saveUninitialized: false,
     resave: false,
-    secret: 'asdknjszsjh485uvep9',
-    // store: new MongoStore(mongoStoreSettings)
+    secret: 'BhMG3w4ECrD3lbXkRU6D8wxC0PTy2D7HCG1sDpVpBaXDFvxUVIx1sHL3ifqCYb6',
     store: new LevelStore()
   }));
 
   app.use(passport.initialize());
   app.use(passport.session());
-  app.use(express.static(__dirname + '/static'));
+  app.use(express.static(`${__dirname}/static`));
 
   auth.init(app);
-  tools.init(app);
+  twitter.init(app);
 
   switch (env) {
     default:
@@ -108,6 +95,16 @@ configureApp(_env);
 /**
  *
  */
-console.log(`${Config.app.title} listening on port %d in %s mode.`, app.get('port'), app.settings.env);
+Logging.log(`${Config.app.title} listening on port ${app.get('port')} in ${app.settings.env} mode.`,
+            Logging.Constants.LogLevel.INFO);
 app.server = app.listen(app.set('port'));
+
+/**
+ *
+ */
+rest.get(Config.VIOLET_BLOCKLIST_URL, {timeout: 5000})
+  .on('success', data => {
+    Logging.log(data, Logging.Constants.LogLevel.VERBOSE);
+    app.blockList = data;
+  });
 
