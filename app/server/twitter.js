40dd4b8dc947a769b01a8599b44e3f4b01536351
Chris Bates-Keegan <chris@evansfinch.co.uk>
2016-08-26 16:01:50 +0100

    - IMPROVED: API queue manager.

--- app/server/twitter.js
+++ app/server/twitter.js
@@ -14,7 +14,69 @@ var Logging = require('./logging');
 var Config = require('./config');
 var Twitter = require('twitter');
 var rest = require('restler');
+var _ = require('underscore');
 
+/**
+ * @param {object} queueItem - All parameters necessary to execute an API call
+ * @return {Promise} - resolves with the queue item (results populated)
+ * @private
+ */
+var _blockTask = queueItem => {
+  var twitter = new Twitter({
+    consumer_key: Config.VIOLET_TW_CONSUMER_KEY,
+    consumer_secret: Config.VIOLET_TW_CONSUMER_SECRET,
+    access_token_key: queueItem.token,
+    access_token_secret: queueItem.tokenSecret
+  });
+
+  return new Promise((resolve, reject) => {
+    twitter.post(queueItem.api, queueItem.params, function(error, data, response) {
+      Logging.log(`Twitter: ${queueItem.api}`, Logging.Constants.LogLevel.INFO);
+      Logging.log(data, Logging.Constants.LogLevel.VERBOSE);
+      if (error) {
+        resolve(false);
+        return;
+      }
+      queueItem.results = data;
+      resolve(queueItem);
+    });
+  });
+};
+
+class TwitterQueueManager {
+  constructor() {
+    this._queue = [];
+    setTimeout(_.bind(this.flushQueue, this), this.Constants.INTERVAL);
+  }
+
+  get Constants() {
+    return {
+      INTERVAL: 30000
+    };
+  }
+
+  addToQueue(queueItem) {
+    this._queue.push(queueItem);
+  }
+
+  flushQueue() {
+    Promise.all(this._queue.map(qi => _blockTask(qi)))
+      .then(Logging.Promise.logArray('Twitter Results: ', Logging.Constants.LogLevel.INFO))
+      // .then(queue => {
+      //   this._queue = queue.filter(qi => qi.repeating);
+      // })
+      .catch(err => Logging.log(err));
+    this._queue = [];
+  }
+}
+
+var _queueManager = new TwitterQueueManager();
+
+/**
+ * @param {object} req - Express request object
+ * @param {object} res - Express response object
+ * @private
+ */
 var _getStatuses = (req, res) => {
   if (!req.user) {
     res.sendStatus(400);
@@ -41,24 +103,6 @@ var _getStatuses = (req, res) => {
   });
 };
 
-var _blockTask = (twitter, profile) => {
-  var params = {
-    screen_name: profile.username,
-    skip_statuses: true
-  };
-
-  return new Promise((resolve, reject) => {
-    twitter.post('blocks/create.json', params, function(error, data, response) {
-      Logging.log(`Blocking: ${params.screen_name}`);
-      if (error) {
-        resolve(false);
-        return;
-      }
-      resolve(true);
-    });
-  });
-};
-
 /**
  *
  * @param {object} req - Express request object
@@ -71,28 +115,19 @@ var _blockAccounts = (req, res) => {
     return;
   }
 
-  Logging.log(`Attempting to block ${req.app.blockList.length} accounts.`, Logging.Constants.LogLevel.INFO);
+  Logging.log(`Scheduling block task for ${req.app.blockList.length} accounts.`, Logging.Constants.LogLevel.INFO);
 
-  var twitter = new Twitter({
-    consumer_key: Config.VIOLET_TW_CONSUMER_KEY,
-    consumer_secret: Config.VIOLET_TW_CONSUMER_SECRET,
-    access_token_key: req.user.token,
-    access_token_secret: req.user.tokenSecret
-  });
-
-  var promises = [];
   req.app.blockList.forEach(b => {
-    promises.push(_blockTask(twitter, b));
-  });
-  Promise.all(promises)
-    .then(Logging.Promise.logProp('Blocked: ', 'length'))
-    .then(results => {
-      results = results.filter(r => r);
-      res.json({err: false, res: results.length});
-    }, err => {
-      Logging.log(err);
-      res.json({err: true, res: err.message});
+    _queueManager.addToQueue({
+      username: req.user.username,
+      api: 'blocks/create.json',
+      params: {screen_name: b.username, skip_statuses: true},
+      token: req.user.token,
+      tokenSecret: req.user.tokenSecret
     });
+  });
+
+  res.json(req.app.blockList.length);
 };
 
 var _getBlocklist = (req, res) => {
@@ -121,6 +156,11 @@ var _loadBlocklist = app => {
       });
 
       twitter.get('users/lookup.json', {screen_name: data.join(',')}, function(error, data, response) {
+        if (error) {
+          Logging.log('Error getting blocklist details', Logging.Constants.LogLevel.ERR);
+          return;
+        }
+
         var list = data.map(p => {
           return {
             id: p.id,
@@ -144,13 +184,14 @@ var _loadBlocklist = app => {
         });
         // Logging.log(list);
         Logging.log(data[0], Logging.Constants.LogLevel.VERBOSE);
-        Logging.log(`Blockist Length: ${data.length}`, Logging.Constants.LogLevel.INFO);
+        Logging.log(`Blocklist Length: ${data.length}`, Logging.Constants.LogLevel.INFO);
         app.blockList = list;
       });
     });
 };
 
 module.exports.init = app => {
+  setTimeout(_loadBlocklist, 60000 * 5, app);
   _loadBlocklist(app);
 
   app.get('/api/twitter/statuses', _getStatuses);
