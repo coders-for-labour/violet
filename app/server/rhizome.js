9c908dbc0997e06374e476e89bf0de8ecf983056
Chris Bates-Keegan <chris@evansfinch.co.uk>
2016-09-09 17:27:24 +0100

    - ADDED: Linked user authentication to Rhizome. - ADDED: First rhizome API helper tool. This will most likely end up in its own repo but here for now while the tyres are kicked. - NOTE: When authenticating against twitter Rhizome manages a user lifecycle. This means that different Rhizome apps can leverage the same user tokens (only once they've authenticated though). Rhizome now supports metadata for apps as well as users. User metadata is not exposed across different apps.

---
+++ app/server/rhizome.js
@@ -0,0 +1,65 @@
+'use strict';
+
+/**
+ * Violet - Social tools for grassroots movements
+ *
+ * @file twitter.js
+ * @description
+ * @module System
+ * @author Chris Bates-Keegan
+ *
+ */
+
+const restler = require('restler');
+const Config = require('./config');
+const Logging = require('./logging');
+
+/**
+ *
+ */
+class Rhizome {
+  findOrCreateUser(user) {
+    return new Promise((resolve, reject) => {
+      var url = `${Config.rhizomeUrl}/user/${user.app}/${user.id}`;
+      Logging.log(url, Logging.Constants.LogLevel.DEBUG);
+      restler.get(url, {query: {token: Config.VIOLET_RHIZOME_APP_TOKEN}})
+        .on('success', data => {
+          Logging.log(data, Logging.Constants.LogLevel.DEBUG);
+
+          if (data === false) {
+            url = `${Config.rhizomeUrl}/user`;
+            restler.post(url, {query: {token: Config.VIOLET_RHIZOME_APP_TOKEN}, data: user})
+              .on('success', data => {
+                Logging.log(data, Logging.Constants.LogLevel.DEBUG);
+                resolve(Object.assign(user, {rhizomeId: data.id}));
+              })
+              .on('error', err => {
+                Logging.log(data, Logging.Constants.LogLevel.ERR);
+                reject(err);
+              });
+          } else {
+            var userId = data.id;
+            url = `${Config.rhizomeUrl}/user/${userId}/${user.app}/token`;
+            restler.put(url, {query: {token: Config.VIOLET_RHIZOME_APP_TOKEN},
+                            data: {token: user.token, tokenSecret: user.tokenSecret}})
+              .on('success', data => {
+                Logging.log(data, Logging.Constants.LogLevel.DEBUG);
+                resolve(Object.assign(user, {rhizomeId: userId}));
+              })
+              .on('error', err => {
+                Logging.log(data, Logging.Constants.LogLevel.ERR);
+                reject(err);
+              });
+          }
+        })
+        .on('error', err => {
+          Logging.log(err, Logging.Constants.LogLevel.ERR);
+        });
+    });
+  }
+}
+
+var _rhizome = new Rhizome();
+module.exports.instance = () => {
+  return _rhizome;
+};
